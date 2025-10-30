d16fdc80f6ed29f97719d123ae3c449d2487a3ae
SumaraNaseem <nsumara524@gmail.com>
2025-10-30 06:46:41 +0500

    add files or deployokljuj

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ const explicitAllowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://water-tool-frontend-2tyw.vercel.app',
-  'https://water-tool-frontend-atgm.vercel.app'
+  'https://water-tool-frontend-atgm.vercel.app',
+  'https://water-tool-frontend-2tyw.vercel.app'
 ].filter(Boolean);
 
 app.use(cors({
